76a9c8188951d7d33f8df21dfedcd575a41f7a22
Diwanabot <156938668+Diwanabot@users.noreply.github.com>
2024-05-10 15:52:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Tahakhanop786/SIDRA-X-MANAGER",
+    "https://github.com/Diwanabot/Nobita",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
